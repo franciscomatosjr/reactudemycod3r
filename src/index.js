e1cb103803d9aa26363af00d57f3f79e1d92d7d6
rairojr <junior.mg@gmail.com>
2021-01-29 13:18:21 -0300

    aula 13

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
+import './index.css'
 import ReactDOM from 'react-dom'
+import React from 'react'
+import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 ReactDOM.render(
-    '<div>Olá React!!!</div>', 
-    document.getElementById("root") 
+    <div>
+        <Primeiro></Primeiro>
+        <ComParametro></ComParametro>
+    </div>, 
+    document.getElementById('root') 
 )
