fcc51e036df58fef890c0c459b2a7451956b8dc0
rairojr <junior.mg@gmail.com>
2021-02-01 13:04:59 -0300

    Aula 14

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
+import Fragmento from './components/basicos/Fragmento'
 
 ReactDOM.render(
     <div>
@@ -10,6 +11,7 @@ ReactDOM.render(
         <ComParametro 
             titulo="Situação do aluno"
             aluno="Pedro Silva" nota={9.3}/>
+        <Fragmento />
     </div>,
     document.getElementById('root') 
 )
