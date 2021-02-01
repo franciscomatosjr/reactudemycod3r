3a1aec51c8169470d94a7b45f2bf219829f5864d
rairojr <junior.mg@gmail.com>
2021-02-01 12:50:56 -0300

    aula13

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import ComParametro from './components/basicos/ComParametro'
 ReactDOM.render(
     <div>
         <Primeiro></Primeiro>
-        <ComParametro></ComParametro>
-    </div>, 
+        <ComParametro 
+            titulo="Situação do aluno"
+            aluno="Pedro Silva" nota={9.3}/>
+    </div>,
     document.getElementById('root') 
 )
